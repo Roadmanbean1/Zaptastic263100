e21dc785a2699d4cb5d05566768aaea2c186d490
Roadmanbean1 <g.thoburn@hotmail.co.uk>
2022-02-20 19:09:25 +0000

    Revert "Automatic scene switch after go back to game button clicked"
    This reverts commit a2413891caa1dd560743575cd1af83abe77ffd1d.

--- Zaptastic My Attempt 2/Zaptastic My Attempt 2/Quiz.swift
+++ Zaptastic My Attempt 2/Zaptastic My Attempt 2/Quiz.swift
@@ -8,18 +8,12 @@
 import UIKit
 
 class Quiz: UIViewController {
-    @IBOutlet weak var backButton: UIButton!
-    
+
     var levelValue: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    }
-    @IBAction func goBackToGame(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
-        let refreshGame = Notification.Name(rawValue: refreshGame1)
-        NotificationCenter.default.post(name: refreshGame, object: nil)
     }
     
 }
